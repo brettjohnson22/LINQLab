d7cf90feda69a07a74b5a1fa5122e937ba85d34f
brettjohnson22 <br.onson@outlook.com>
2023-04-12 16:41:48 -0500

    Fixed Queries

--- LINQLab/Program.cs
+++ LINQLab/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Problems prob = new Problems();
+            prob.RunLINQQueries();
         }
     }
 }
